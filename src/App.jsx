cfe794dd6898897996c6bb4d2cbeb51b00d7a2a3
Jason Howell <howell.jasong@gmail.com>
2025-03-16 16:51:44 -0500

    using the data.js file commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList)
 
+  console.log(puppies);
+  
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      </div> */}
     </>
   );
 }
