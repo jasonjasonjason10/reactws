a0be11324ce3435474ac30bf997094ca4858e13d
Jason Howell <howell.jasong@gmail.com>
2025-03-16 21:56:55 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,37 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {puppyList} from './data.js'
+import { puppyList } from "./data.js";
+import './mystyle.css'
 
 function App() {
-  // const [count, setCount] = useState(0);
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log(puppies);
-  
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <>
-      <h1>Vite + React</h1>
-      <div>
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
+      {/* <h1>Vite + React</h1> */}
+      <div id="mytext">
+        {puppies.map((puppy) => (
+          <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        ))}
       </div>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div> */}
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
